eb54a265e89f43dd90a3e89e08e9fc3feb6ab67e
Loz <tuanchu24112004@gmail.com>
2024-07-22 18:02:39 +0700

    Search

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation("com.airbnb.android:lottie:6.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //For getting json data
+    implementation("com.android.volley:volley:1.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/example/weatherapp/location/LocationCord.java
@@ -0,0 +1,7 @@
+package com.example.weatherapp.location;
+
+public class LocationCord {
+    public static final String lat = "";
+    public static final String lon = "";
+    public static final String API_KEY = "e5afb6abedc33f32a139cf17a8921af6";
+}
